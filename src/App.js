cd4417a5ce63c9761f3ed9c2ecc9cc9d0a0d7026
Aline Marques <79379302+LineMeo@users.noreply.github.com>
2022-07-12 16:19:32 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// Faça login no CodeSandbox
+
+// Crie um novo sandbox em REACT
+
+// Crie um Component de function e retorne uma frase na tela
+
+import React from "react";
+
+export default function Ola() {
+  return (
+    <div>
+      <h1>Trabalhando com Funções em React no módulo 2 - VnW</h1>
+    </div>
+  );
+}
